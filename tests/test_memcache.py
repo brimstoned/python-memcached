b1020001ea6d9b4a8212c5fe13803d898413f873
pdisessa <Peter_Di_Sessa@discovery.com>
2018-07-10 09:57:05 -0500

    Adding a test to verify the flags are overwritten in object instance

--- tests/test_memcache.py
+++ tests/test_memcache.py
@@ -223,6 +223,25 @@ class TestMemcache(unittest.TestCase):
             "'NOT_FOUND'\n"
         )
 
+    def test_flag_overwrite_on_instance(self):
+        """Testing that flags are overwritten in object instance"""
+        servers = ["127.0.0.1:11211"]
+        flags_to_overwrite = {
+            Client._FLAG_INTEGER: 1 << 3,     # 0000 0000 0000 1000 == 8
+            Client._FLAG_COMPRESSED: 1 << 1,  # 0000 0000 0000 0010 == 2
+        }
+        another_mc = Client(servers, flags_to_overwrite=flags_to_overwrite, debug=1)
+
+        self.assertEqual(another_mc._FLAG_INTEGER, 1 << 3)
+        self.assertEqual(another_mc._FLAG_COMPRESSED, 1 << 1)
+        self.assertEqual(self.mc._FLAG_INTEGER, 1 << 1)
+        self.assertEqual(self.mc._FLAG_COMPRESSED, 1 << 3)
+        self.assertEqual(Client._FLAG_INTEGER, 1 << 1)
+        self.assertEqual(Client._FLAG_COMPRESSED, 1 << 3)
+
+        another_mc.flush_all()
+        another_mc.disconnect_all()
+
     @mock.patch.object(_Host, 'send_cmd')  # Don't send any commands.
     @mock.patch.object(_Host, 'readline')
     def test_touch_unexpected_reply(self, mock_readline, mock_send_cmd):
